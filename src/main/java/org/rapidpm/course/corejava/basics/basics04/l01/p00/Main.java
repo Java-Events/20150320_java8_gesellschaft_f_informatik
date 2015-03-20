dd69fabe1667e79e518e634107eac61bf2b7054e
Sven Ruppert <sven.ruppert@gmail.com>
2015-03-20 12:47:39 +0100

    inner classes

--- src/main/java/org/rapidpm/course/corejava/basics/basics04/l01/p00/Main.java
+++ src/main/java/org/rapidpm/course/corejava/basics/basics04/l01/p00/Main.java
@@ -4,4 +4,34 @@ package org.rapidpm.course.corejava.basics.basics04.l01.p00;
  * Created by sven on 20.03.15.
  */
 public class Main {
+
+    public static class ChildA{
+        public void workA(){
+            System.out.println( this.getClass().getSimpleName());
+        }
+        public void workB(){
+            System.out.println(ChildA.class.getSimpleName());
+        }
+    }
+    public class ChildB{
+        public void workA(){
+            System.out.println( this.getClass().getSimpleName());
+        }
+        public void workB(){
+            System.out.println(ChildA.class.getSimpleName());
+        }
+    }
+
+
+
+
+
+    public static void main(String[] args) {
+        ChildA childA = new ChildA();
+        childA.workA();
+        childA.workB();
+    }
+
+
+
 }
--- src/main/java/org/rapidpm/course/corejava/basics/basics04/l01/p00/OuterMain.java
+++ src/main/java/org/rapidpm/course/corejava/basics/basics04/l01/p00/OuterMain.java
@@ -4,4 +4,9 @@ package org.rapidpm.course.corejava.basics.basics04.l01.p00;
  * Created by sven on 20.03.15.
  */
 public class OuterMain {
+    public static void main(String[] args) {
+        Main.ChildA childA = new Main.ChildA();
+        childA.workA();
+        childA.workB();
+    }
 }
