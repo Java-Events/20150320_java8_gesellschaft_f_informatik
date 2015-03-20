4908934db5ffa8a5655c2dee3816a017e390d553
Johannes Nicolai <jnicolai@collab.net>
2015-03-20 12:53:11 +0100

    inner class

---
+++ src/main/java/p08/InnerClasses.java
@@ -0,0 +1,11 @@
+package p08;
+
+import org.rapidpm.course.corejava.basics.basics04.l01.p00.Main;
+
+public class InnerClasses {
+	public static void main(String[] args) {
+		Main main = new Main();
+		main.new ChildB();
+	}
+
+}
